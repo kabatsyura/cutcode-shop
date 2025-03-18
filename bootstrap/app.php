7bde9ab8394c094523849b7757633caee2b627da
kabatsyura <dmitriy.kabatsyura@gmail.com>
2025-03-18 11:19:22 +0300

    Register RouteServiceProvider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->group('web', [
+            'throttle:global',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
